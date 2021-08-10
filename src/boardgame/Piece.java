5ddfe464fdb55b441301333db2a5ee6160b8362c
Jonas Garcia Rosa <jgarcia.rosa@gmail.com>
2021-08-10 20:01:35 -0300

    comentários nas classes Piece e Board!

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -10,9 +10,10 @@ public class Piece {
 
 	public Piece(Board board) {
 		this.board = board;
+	  //position = null; {comentado pois se não explicitar a inicialização o java irá fazer automaticamente!}
 	}
 
-
+	//protected para apenas classes da mesma camada{pacote/package} e/ou respectivas subclasses acessem o tabuleiro associado a uma peça
 	protected Board getBoard() {
 		return board;
 	}
