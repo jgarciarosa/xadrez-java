d6d4b8f6339d7cd5f690fd7b5358cd07800781b3
Jonas Garcia Rosa <jgarcia.rosa@gmail.com>
2021-08-10 11:20:59 -0300

    Classe Position

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position position = new Position(3, 3);
+		System.out.println(position);
+
+	}
+
+}
