cdca023268adb5c6bc50f63dbda666b8ad10be70
Jonas Garcia Rosa <jgarcia.rosa@gmail.com>
2021-08-16 10:22:50 -0300

    desenvolvido possiveis movimentos da torre R, classes: Positiom, ChessMatch, ChessPiece e Rook!

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
 public abstract class ChessPiece extends Piece{
 	
@@ -24,6 +25,11 @@ public abstract class ChessPiece extends Piece{
 
 	public void setMoveCount(int moveCount) {
 		this.moveCount = moveCount;
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;		
 	}
 
 	
