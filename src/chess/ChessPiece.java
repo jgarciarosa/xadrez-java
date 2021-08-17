edf9bc7454f65e944d496c9d4c8a85cf18bf6738
Jonas Garcia Rosa <jgarcia.rosa@gmail.com>
2021-08-17 11:42:41 -0300

    começo dev contagem de movimentos da peça

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -9,22 +9,29 @@ public abstract class ChessPiece extends Piece{
 	private Color color;
 	private int moveCount;
 	
-	public ChessPiece(Board board, Color color, int moveCount) {
+	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
-		this.moveCount = moveCount;
 	}
 
 	public Color getColor() {
 		return color;
 	}
 	
-	public ChessPosition getChessPosition() {
-		return ChessPosition.fromPosition(position);
-	}
-
 	public int getMoveCount() {
 		return moveCount;
+	}
+	
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+	
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
 	}
 
 	public void setMoveCount(int moveCount) {
